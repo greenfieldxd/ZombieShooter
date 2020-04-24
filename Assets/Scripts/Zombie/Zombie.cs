4853f77183adafb48061bf95b784e36a66832f30
greenfieldxd <monsterbig53@gmail.com>
2020-04-24 22:37:26 +0300

    not full

---
+++ Assets/Scripts/Zombie/Zombie.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    [Header("AI config")]
+    public float followDistance;
+    public float attackDistance;
+
+    [Header("Attack config")]
+    public float attackRate;
+    public float damage;
+
+    
+
+    enum ZombieStates
+    {
+        STAND,
+        MOVE,
+        ATTACK
+    }
+
+    ZombieStates activeState;
+
+    Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+
+        ChangeState(ZombieStates.STAND);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateState();
+    }
+
+    void UpdateState()
+    {
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+        switch (activeState)
+        {
+            case ZombieStates.STAND:
+                //DO STAND
+                break;
+            case ZombieStates.MOVE:
+                //DO MOVE
+                break;
+            case ZombieStates.ATTACK:
+                //DO ATTACK
+                break;
+        }
+    }
+
+    void ChangeState(ZombieStates newState)
+    {
+        activeState = newState;
+        switch (activeState)
+        {
+            case ZombieStates.STAND:
+                //DO STAND
+                break;
+            case ZombieStates.MOVE:
+                //DO MOVE
+                break;
+            case ZombieStates.ATTACK:
+                //DO ATTACK
+                break;
+        }
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/Zombie/ZombieMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMovement : MonoBehaviour
+{
+
+    public float speed;
+
+    //other gameObjects
+    Player player;
+
+    //components on the same gameObject
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 direction = player.transform.position - transform.position;
+        rb.velocity = direction.normalized * speed;
+    }
+}
